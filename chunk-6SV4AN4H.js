e03c0c0b27be7fec5d6a2774d3f63c232454ab5b
iamowaisazam <iamowaisazam@gmail.com>
2024-09-09 02:53:47 +0500

    update_code

---
+++ chunk-6SV4AN4H.js
@@ -0,0 +1,1 @@
+import{Oa as i,Pa as a,Va as r,W as o,_a as s}from"./chunk-RSGFSHSU.js";import"./chunk-4CLCTAJ7.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-home"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"home works!"),a())}});let t=e;return t})();export{d as HomeComponent};
--- chunk-OPEMN5ZH.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var t=[{path:"",data:{title:"Home"},loadComponent:()=>import("./chunk-LUL5ECAF.js").then(o=>o.HomeComponent)}];export{t as routes};
